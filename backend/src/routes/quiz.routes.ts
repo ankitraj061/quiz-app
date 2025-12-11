f40d055bb7e058afbb284306a8a7f99fc1b81751
SOFIKUL SK <148935407+sofikulsk02@users.noreply.github.com>
2025-12-11 21:42:43 +0530

    Added data export functionality

--- backend/src/routes/quiz.routes.ts
+++ backend/src/routes/quiz.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import { asyncHandler } from "../utils/asyncHandler";
 import { QuizController } from "../controllers/quiz.controller";
-import { ZCreateQuestion, ZCreateQuiz, ZQuestionUpdate, ZQuizUpdate } from "../types/quiz.types";
+import {
+  ZCreateQuestion,
+  ZCreateQuiz,
+  ZQuestionUpdate,
+  ZQuizUpdate,
+} from "../types/quiz.types";
 import { validate } from "../middlewares/validate.middleware";
 import { authenticateToken } from "../middlewares/auth.middleware";
 import { roleRequired } from "../middlewares/requiredRole.middleware";
@@ -10,34 +15,42 @@ const router = Router();
 
 router.use(authenticateToken, roleRequired("ADMIN"));
 
-router.route("/")
-    .post(validate(ZCreateQuiz), authenticateToken, asyncHandler(QuizController.createQuiz));
+router
+  .route("/")
+  .post(
+    validate(ZCreateQuiz),
+    authenticateToken,
+    asyncHandler(QuizController.createQuiz)
+  );
 
-router.route("/")
-    .get(asyncHandler(QuizController.getAllQuiz));
-    
-router.route("/leaderboard/:quizId")
-    .get(asyncHandler(QuizController.getApplicantsOfQuiz));
+router.route("/").get(asyncHandler(QuizController.getAllQuiz));
 
+router
+  .route("/leaderboard/:quizId")
+  .get(asyncHandler(QuizController.getApplicantsOfQuiz));
 
-router.route("/question")
-    .post(validate(ZCreateQuestion), asyncHandler(QuizController.createQuestion));
+router
+  .route("/question")
+  .post(validate(ZCreateQuestion), asyncHandler(QuizController.createQuestion));
 
-router.route("/question/:questionId")
-    .delete(asyncHandler(QuizController.deleteQuestion));
+router
+  .route("/question/:questionId")
+  .delete(asyncHandler(QuizController.deleteQuestion));
 
-router.route("/question/:questionId")
-    .patch(validate(ZQuestionUpdate), asyncHandler(QuizController.updateQuestion));
+router
+  .route("/question/:questionId")
+  .patch(
+    validate(ZQuestionUpdate),
+    asyncHandler(QuizController.updateQuestion)
+  );
 
 // get quiz with questions
-router.route("/:quizId")
-    .get(asyncHandler(QuizController.getCompleteQuiz));
+router.route("/:quizId").get(asyncHandler(QuizController.getCompleteQuiz));
 
-router.route("/:quizId")
-    .delete(asyncHandler(QuizController.deleteQuiz));
-    
-router.route("/:quizId")
-    .patch(validate(ZQuizUpdate), asyncHandler(QuizController.updateQuiz));
+router.route("/:quizId").delete(asyncHandler(QuizController.deleteQuiz));
 
+router
+  .route("/:quizId")
+  .patch(validate(ZQuizUpdate), asyncHandler(QuizController.updateQuiz));
 
 export default router;
